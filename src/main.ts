6939d96b3c5658c494f43bc9bdfcee937d99768e
Marco Aristin Djembissie <kuidjamarco@gmail.com>
2022-08-19 13:30:30 +0100

    [FIXED: JSON found unexpected caracter]

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
       optionsSuccessStatus: 204,
     },
   });
-  await app.listen(process.env.PORT ||4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
