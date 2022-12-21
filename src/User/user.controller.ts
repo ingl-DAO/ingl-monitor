4cf3ee1ece2579416776c6f81e051aa4f69b7b61
Marco Kuidja <kuidjamarco@gmail.com>
2022-12-21 20:08:39 +0100

    Added user module and implemented User findAll

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,20 +1,27 @@
 import { Controller, Get, Post, SetMetadata, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/Auth/jwt/jwt-auth.guard';
-import { UserPostDto, UserService } from 'src/User/user.service';
+import { CollectionName, User, UserPostDto } from 'src/Mongo/mongo.dto';
+import { MongoService } from 'src/Mongo/mongo.service';
 
 @Controller('users')
 @UseGuards(JwtAuthGuard)
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private mongoService: MongoService) {}
 
   @Get('all')
   async getUsers() {
-    return this.userService.findAll();
+    const users = await this.mongoService.findAll<User>(
+      CollectionName.BetaUsers
+    );
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return users.map(({ password, ...user }) => {
+      return user;
+    });
   }
 
   @Post('new')
   @SetMetadata('isAdmin', true)
   async addNewUser(newUser: UserPostDto) {
-    return this.userService.insert(newUser);
+    return this.mongoService.insert(CollectionName.BetaUsers, { ...newUser });
   }
 }
