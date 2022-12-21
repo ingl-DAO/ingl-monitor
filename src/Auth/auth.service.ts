1e27aec75bf414b1955fc3049e5ddc1d6d4f122b
Marco Kuidja <kuidjamarco@gmail.com>
2022-12-21 15:04:17 +0100

    disable unused variable

--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -13,6 +13,7 @@ export class AuthService {
   async validateUser(username: string, password: string) {
     const user = await this.userService.findOne(username);
     if (user && bcrypt.compareSync(password, user.password)) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...userInfo } = user;
       return userInfo;
     }
